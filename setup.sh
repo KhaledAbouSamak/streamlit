8ab12d34e4cc1f778cd7ea12e3ba17e6f8319e1f
Khaled Abou Samak <56739490+KhaledAbouSamak@users.noreply.github.com>
2020-05-08 22:03:16 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"khalidsamak2005@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
