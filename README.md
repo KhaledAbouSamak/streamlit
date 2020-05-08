96d6a4bc752fe2f218eaa0fdeec37a1fc02a238b
Khaled Abou Samak <56739490+KhaledAbouSamak@users.noreply.github.com>
2020-05-08 14:19:49 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+Example of my stramlit deployment to Heroku
+
+# Required files:
+1 setup.sh
+2 Procfile
+3 requirements.txt
+`find dependecies automatic
+pipreqs projectpath
---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def main():
+	""" A simple Iris EDA app"""
+	st.title('Iris EDA with Streamlit')
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
